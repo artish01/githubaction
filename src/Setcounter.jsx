c1759d0d2ad15ae48e85dca6448c54416e99a6c4
Artish Kumar <84556749+artish01@users.noreply.github.com>
2023-04-26 12:08:12 +0530

    Create Setcounter.jsx

---
+++ src/Setcounter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Setcounter from './Setcounter'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Counter</h1>
+    <div className='card'>
+    <Setcounter/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
